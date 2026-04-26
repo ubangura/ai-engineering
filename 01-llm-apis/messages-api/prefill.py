37b8112e0dc9fc0a3ef4c1c015468a656157bfba
Unisa Bangura <unisa.b@outlook.com>
2026-04-26 08:37:40 -0400

    feat: add prefill Claude reponse script

---
+++ 01-llm-apis/messages-api/prefill.py
@@ -0,0 +1,16 @@
+from anthropic import Anthropic
+
+from api_config import dev_config
+
+client = Anthropic()
+
+message = client.messages.create(
+    model=dev_config.model,
+    max_tokens=2,
+    messages=[
+        {"role": "user", "content": "What is English for the word كتاب"},
+        {"role": "assistant", "content": 'كتاب in English is "'},
+    ],
+)
+
+print(message)
