cae1f7e95a151d435d81969b45ecd7afe85c80e6
Unisa Bangura <unisa.b@outlook.com>
2026-04-30 19:43:22 -0400

    feat: add system prompt script for math tutoring

---
+++ 01-llm-apis/messages-api/system_prompt.py
@@ -0,0 +1,16 @@
+from anthropic import Anthropic
+
+from messaging import add_user_message, chat
+
+client = Anthropic()
+
+system = """
+You are a patient math tutor.
+Do not directly answer a student's questions.
+Guide them to a solution step-by-step.
+"""
+
+messages = []
+add_user_message(messages, "How do I solve 5x + 3 = 2 for x?")
+message = chat(messages, system)
+print(message)
